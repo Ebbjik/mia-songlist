35271b164aed85168b6946d83deb3ea0febb0897
DESKTOP-78RNM5S\admin <2772272916@qq.com>
2025-03-23 17:55:47 +0800

    引入less 添加手机端样式

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,22 @@
 <script>
     // ... existing code ...
-    import { browser } from "$app/environment";
-    let deviceClass = "";
-    if (browser) {
-        if (/Mobi|Android/i.test(navigator.userAgent)) {
-            deviceClass = "mobile";
-        } else {
-            deviceClass = "desktop";
-        }
-    }
-    // ... existing code ...
 
     export let countnum = 24;
 </script>
 
-<main class={deviceClass}>
-    <div class="introduction">
-        <img class="mia-head" alt="mia" src="/mia-head.jpeg" />
-        <p class="introduce">丘米芽和她擅长的</p>
-        <span class="total">
-            {countnum}
-        </span>
-        <p class="introduce">首歌曲</p>
+<main class="container">
+    <div class="profile">
+        <img class="profile__avatar" alt="mia" src="/mia-head.jpeg" />
+        <div class="profile__info">
+            <p class="profile__text">丘米芽和她擅长的</p>
+            <span class="profile__count">
+                {countnum}
+            </span>
+            <p class="profile__text">首歌曲</p>
+        </div>
     </div>
     <div class="right">
-        <div class="selector">这是选择器部分</div>
-        <div class="song_list">这是歌单部分ceshi</div>
+        <div class="song-selector">这是选择器部分</div>
+        <div class="song-list">这是歌单部分ceshi</div>
     </div>
 </main>

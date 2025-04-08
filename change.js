053230416176adc28430c133599dc6af6febd635
Ebbjik <2772272916@qq.com>
2025-04-08 16:58:56 +0800

    修改脚本

--- change.js
+++ change.js
@@ -11,16 +11,20 @@ const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
 
 
 // 遍历每首歌
-data.中文.forEach((song) => {
-    // 检查歌名是否以【**SC】结尾
-    const match = song["歌名"] && song["歌名"].match(/【(\d+)SC】$/);
-    if (match) {
-        // 添加SC档位字段
-        song["SC档位"] = parseInt(match[1], 10);
-        // 删除歌名后面的【】部分
-        song["歌名"] = song["歌名"].replace(/【\d+SC】$/, '');
-        console.log(song)
-    }
+
+Object.keys(data).forEach(category => {
+    console.log(`Category: ${category}`);
+    data[category].forEach((song) => {
+            // 检查歌名是否以【**SC】结尾
+            const match = song["歌名"] && song["歌名"].match(/【(\d+)SC】$/);
+            if (match) {
+                // 添加SC档位字段
+                song["SC档位"] = parseInt(match[1], 10)+'元';
+                // 删除歌名后面的【】部分
+                song["歌名"] = song["歌名"].replace(/【\d+SC】$/, '');
+                console.log(song)
+            }
+    });
 });
 
 // 将更新后的数据写回songlist.json文件
--- eslint.config.js
+++ eslint.config.js
@@ -14,23 +14,24 @@ export default ts.config(
   ...svelte.configs.recommended,
   {
     languageOptions: {
-	  globals: {
-	    ...globals.browser,
-	    ...globals.node
-	  }
-	}
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
   },
   {
     files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
     ignores: ["eslint.config.js", "svelte.config.js"],
 
     languageOptions: {
-	  parserOptions: {
-	    projectService: true,
-	    extraFileExtensions: ['.svelte'],
-	    parser: ts.parser,
-	    svelteConfig
-	  }
-	}
+      parserOptions: {
+        projectService: true,
+        extraFileExtensions: ['.svelte'],
+        parser: ts.parser,
+        svelteConfig
+      }
+    }
   }
 );
+

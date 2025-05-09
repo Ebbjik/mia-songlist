6775131e59151f4993e44f47f22e941421c1b328
Ebbjik <2772272916@qq.com>
2025-05-09 21:56:55 +0800

    修改表格样式

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,14 +13,15 @@
 </svelte:head>
 
 <main class="container">
-  <div class="profile">
-    <img class="profile__avatar" alt="mia" src="/mia-head.jpeg" />
-    <div class="profile__info">
-      <p class="profile__text">丘米芽和她擅长的</p>
-      <span class="profile__count">
-        {countnum}
-      </span>
-      <p class="profile__text">首歌曲</p>
+    <div class="profile">
+        <img class="profile__avatar" alt="mia" src="/mia-head.jpeg" />
+        <div class="profile__info">
+            <p class="profile__text">丘米芽和她擅长的</p>
+            <span class="profile__count">
+                {countnum}
+            </span>
+            <p class="profile__text">首歌曲</p>
+        </div>
     </div>
     <div class="right">
         <div class="song-selector">这是选择器部分</div>

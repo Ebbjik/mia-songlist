3fc79c134b230faa30ec961e1e3e28161ec842b7
Ebbjik <2772272916@qq.com>
2025-05-10 13:14:49 +0800

    应用储存

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,45 @@
 <script>
-    let { data } = $props();
+  let { data } = $props();
 
-    const songlist = data.props.data;
+  const songlist = data.props.data;
 
-    const countnum = data.props.countnum;
+  const countnum = data.props.countnum;
 </script>
 
 <svelte:head>
-    <title>mia的歌单</title>
+  <title>mia的歌单</title>
 </svelte:head>
 
 <main class="container">
-    <div class="profile">
-        <img class="profile__avatar" alt="mia" src="/mia-head.jpeg" />
-        <div class="profile__info">
-            <p class="profile__text">丘米芽和她擅长的</p>
-            <span class="profile__count">
-                {countnum}
-            </span>
-            <p class="profile__text">首歌曲</p>
-        </div>
+  <div class="profile">
+    <img class="profile__avatar" alt="mia" src="/mia-head.jpeg" />
+    <div class="profile__info">
+      <p class="profile__text">丘米芽和她擅长的</p>
+      <span class="profile__count">
+        {countnum}
+      </span>
+      <p class="profile__text">首歌曲</p>
     </div>
-    <div class="right">
-        <div class="song-selector">这是选择器部分</div>
-        <div class="song-list">
-            <table>
-                <thead>
-                    <tr>
-                        <th>歌名</th>
-                        <th>语言</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {#each songlist as song (song.歌名 + song["歌手/翻唱版本"])}
-                        <tr>
-                            <td>{song.歌名}</td>
-                            <td>{song.语言}</td>
-                        </tr>
-                    {/each}
-                </tbody>
-            </table>
-        </div>
+  </div>
+  <div class="right">
+    <div class="song-selector">这是选择器部分</div>
+    <div class="song-list">
+      <table>
+        <thead>
+          <tr>
+            <th>歌名</th>
+            <th>语言</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each songlist as song (song.歌名 + song["歌手/翻唱版本"])}
+            <tr>
+              <td>{song.歌名}</td>
+              <td>{song.语言}</td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
     </div>
+  </div>
 </main>
